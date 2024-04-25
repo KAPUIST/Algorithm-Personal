0b4cb4f5ccd7c677b4529e1c72d7228c45610ee3
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-04-25 12:55:53 +0900

    중앙값구하기

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-function ad(x, a, z) {
-  console.log(x + a + z);
+function solution(array) {
+  let sortedArray = array.sort((a, b) => a - b);
+  let index = Math.floor(sortedArray.length / 2);
+  console.log(sortedArray);
+  console.log(sortedArray[index]);
 }
 
-console.log(ad(1, 4, 5));
-function ad(x, a, z) {
-  console.log(x - a - z);
-}
+solution([1, 2, 7, 10, 11, 1, 23, 3, 4, 25425, 12]);
