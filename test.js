e07bd267f6d110e773599d1be3b8c7ec2dc79b60
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-05-21 09:49:45 +0900

    Algo

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
-function solution(n) {
-  let num = Math.sqrt(n);
-  console.log(num);
-  if (num % 1 !== 0) return -1;
-  return (num + 1) * (num + 1);
+function solution(number) {
+  let count = 0; // 삼총사의 수를 셀 변수
+  function find(start, comb) {
+    if (comb.length === 3) {
+      if (comb[0] + comb[1] + comb[2] === 0) {
+        count++;
+      }
+      return;
+    }
+
+    for (let i = start; i < number.length; i++) {
+      find(i + 1, comb.concat(number[i]));
+    }
+  }
+  find(0, []);
+  return count;
 }
-solution(121);
+
+// 테스트 예제
+console.log(solution([-2, 3, 0, 2, -5])); // 2
