94b7878407dad026351afcef5c68b7cb5a17aaa8
Luke Son <91464689+KAPUIST@users.noreply.github.com>
2024-06-13 09:42:40 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.37 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,30 +1,23 @@
 function solution(number, limit, power) {
-    var answer = 0;
-    let divisor = [];
-    //1부터 number까지의 수의 모든 약수가 필요함 
-    //약수 구하기
-    for(let j = 1; j <= number; j++){
-        divisor.push(j)
-    }
-    //[1,2,3,4,5]
-    //새로운 배열을 리턴하도록
-       divisor = divisor.map((el) => {
-    let tempArr = [];
-    for (let i = 1; i <= Math.sqrt(el); i++) {
-      if (el % i === 0) {
-        tempArr.push(i);
-        if (el / i != i) tempArr.push(el / i);
+  let answer = 0;
+
+  // 약수의 개수를 계산하는 함수
+  const getDivisorCount = (num) => {
+    let divisorCount = 0;
+    for (let i = 1; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        divisorCount++; // i는 num의 약수
+        if (num / i != i) divisorCount++; 
       }
     }
-    if (limit < tempArr.length) {
-      return power;
-    } else {
-      return tempArr.length;
-    }
-  });
-    answer = divisor.reduce((acc,cur) => {
-        return acc+cur
-    })
-    //[1,2,2,2,2,2]
-    return answer;
+    return divisorCount;
+  };
+
+ 
+  for (let j = 1; j <= number; j++) {
+    let count = getDivisorCount(j);
+    answer += count > limit ? power : count;
+  }
+
+  return answer;
 }
